9a79e35711f40eda5efe199d52329d29209998a8
rogeriodevelop <rogeriodevelop@gmail.com>
2023-06-22 22:41:29 -0300

    primeiros commit

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,10 +1,36 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        analisarCandidato(1900);
-        analisarCandidato(2200);
-        analisarCandidato(2000);
+        
+    }
+
+    static void selecaoCandidatos(){
+
+        String [] candidatos = {"FELIPE", "MARCIA","JULIA","PAULO","AUGUSTO","MONICA","FABRICIO","MIRELA", "DANIELA", "JOELMA"};
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+
+        while(candidatosSelecionados <5 && candidatoAtual < candidatos.length){
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O candidato "+ candidato + "solicitou este valor de salario "+ salarioPretendido);
+            if(salarioBase >= salarioPretendido){
+                System.out.println("O candidato "+ candidato + "foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatoAtual++;
+        }
+    }
+
+    static double valorPretendido(){
+
+        return ThreadLocalRandom.current().nextDouble(1800,2200);
     }
 
     static void analisarCandidato(double salarioPretendido){
